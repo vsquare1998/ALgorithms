68058a16f5d267ca3d69a0a7bfd77c2d9447e1c4
Vaibhav Vishal <vaibhavvishal1244@gmail.com>
2020-06-19 10:55:50 +0530

    first commit

--- fibonacci_number_1.py
+++ fibonacci_number_1.py
@@ -1,9 +1,10 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    lis = [0,1]
+    
+    for i in range(2,n+1):
+        lis.append(lis[-1]+lis[-2])
+    return lis[-1]
 
 n = int(input())
 print(calc_fib(n))
